8f865360a44aff0ceb0bff19a4dbce7a3841fb89
Daniil Klochkov <kldaniil@gmail.com>
2026-03-07 17:21:00 +0500

    fixed artist_profile model slug field generate and save.

--- users/models/artist_social.py
+++ users/models/artist_social.py
@@ -3,8 +3,6 @@ from django.db import models
 
 from ..constants import (
     ARTIST_LINK_LABEL_MAX_LENGTH, ARTIST_LINK_LABEL_MIN_LENGTH,
-    ARTIST_LINK_FIELD_MAX_LENGTH, ARTIST_LINK_FIELD_MIN_LENGTH,
-    ARTIST_LINK_TYPE_MAX_LENGTH
 )
 from .abstract import ActivatableModel, TimestampModel
 
