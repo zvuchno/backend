844f76028df3e51ab60099b85e2067ed083f7ead
Pavel Kulichenko <pavel.kulichenko@gmail.com>
2026-03-12 15:57:10 +0500

    refactor(models): refactor Album, Genre, and Track models

--- store/models/track.py
+++ store/models/track.py
@@ -1,21 +1,29 @@
+"""
+Модель музыкального трека, входящего в состав альбома.
+
+Связан с альбомом и пользователем-владельцем.
+"""
+
 from decimal import Decimal
 
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.core.validators import MinValueValidator
+from django.core.validators import FileExtensionValidator, MinValueValidator
 
 from .album import Album
-from ..constants import (
+from store.constants import (
+    ALLOWED_AUDIO_EXTENSIONS,
     MAX_CHAR_LENGTH,
     MAX_PRICE_DIGITS,
     MAX_STR_LENGTH,
     PRICE_DECIMAL_PLACES,
 )
+from users.models.abstract import ActivatableModel, TimestampModel
 
 User = get_user_model()
 
 
-class Track(models.Model):
+class Track(ActivatableModel, TimestampModel):
     """Музыкальный трек в составе альбома."""
 
     name = models.CharField('Название', max_length=MAX_CHAR_LENGTH)
@@ -28,8 +36,23 @@ class Track(models.Model):
     audio_file = models.FileField(
         'Файл трека',
         upload_to='tracks/',
+        validators=[FileExtensionValidator(
+            allowed_extensions=ALLOWED_AUDIO_EXTENSIONS
+        )],
         help_text='Аудиофайл'
         )
+    duration = models.PositiveIntegerField(
+        'Длительность',
+        null=True,
+        blank=True,
+        help_text='Длительность трека в секундах'
+    )
+    track_number = models.PositiveIntegerField(
+        'Номер трека',
+        null=True,
+        blank=True,
+        help_text='Порядок трека в альбоме'
+    )
     individual_price = models.DecimalField(
         'Цена трека',
         max_digits=MAX_PRICE_DIGITS,
@@ -38,14 +61,13 @@ class Track(models.Model):
         default=Decimal('0.00'),
         help_text='Цена, руб.'
     )
-    allow_fans_to_pay_more = models.BooleanField(
+    allow_fans_overpay = models.BooleanField(
         'Разрешить платить больше',
         default=False,
         help_text='Если включено, фанаты смогут заплатить больше стоимости.'
     )
     lyrics = models.TextField('Текст трека', blank=True, default='')
-    created_at = models.DateTimeField('Дата добавления', auto_now_add=True)
-    user = models.ForeignKey(
+    owner = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='tracks',
@@ -54,8 +76,15 @@ class Track(models.Model):
 
     class Meta:
         verbose_name = 'трек'
-        verbose_name_plural = 'Треки'
-        ordering = ('album', 'name',)
+        verbose_name_plural = 'треки'
+        ordering = ('album', 'track_number', 'name',)
+        constraints = [
+            models.UniqueConstraint(
+                fields=['album', 'track_number'],
+                name='unique_track_number_per_album')
+        ]
 
     def __str__(self):
+        if self.track_number is not None:
+            return f'{self.track_number}. {self.name[:MAX_STR_LENGTH]}'
         return self.name[:MAX_STR_LENGTH]
