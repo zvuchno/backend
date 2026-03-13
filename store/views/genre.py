4393e9750b2578618627513dd662770189f4017d
Куличенко Павел <pavel.kulichenko@gmail.com>
2026-03-13 11:36:19 +0500

    Feat/20 setup api documentation

--- store/views/genre.py
+++ store/views/genre.py
@@ -1,9 +1,17 @@
+"""
+ViewSet для работы с моделью Genre.
+
+Предоставляет полный цикл CRUD-операций для модели Genre.
+"""
+
 from rest_framework import viewsets
 
 from store.models import Genre
+from store.schema import genre_schema
 from store.serializers import GenreSerializer
 
 
+@genre_schema
 class GenreViewSet(viewsets.ModelViewSet):
     """API для работы с жанрами."""
 
