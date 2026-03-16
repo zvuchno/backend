717c626ef6c5f19f2baaa56d8391846f74b0235d
kldaniil <kldaniil@gmail.com>
2026-03-16 23:17:31 +0500

    Revert "fix listener model (#55)"
    This reverts commit c686291f9728be5ccd853f12829f1fd5152da516.

--- users/models/listener_profile.py
+++ users/models/listener_profile.py
@@ -4,8 +4,6 @@ from django.conf import settings
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
-
-from users.constants import FULL_NAME_FIELD_MAX_LENGTH
 from .abstract.activatable_model import ActivatableModel
 from .abstract.timestamp_model import TimestampModel
 
@@ -28,15 +26,10 @@ class ListenerProfile(ActivatableModel, TimestampModel):
         help_text='Номер телефона',
         unique=True,
     )
-    full_name = models.CharField(
-        'Имя и фамилия',
-        max_length=FULL_NAME_FIELD_MAX_LENGTH,
-    )
 
     class Meta:
         verbose_name = 'слушатель'
         verbose_name_plural = 'слушатели'
-        ordering = ('-created_at',)
 
     def __str__(self):
         return self.user.email
