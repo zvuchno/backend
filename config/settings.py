8e99afe09b94362706c9cbe99b9a81c16ca73b75
Daniil Klochkov <kldaniil@gmail.com>
2026-03-18 18:32:59 +0500

    load_dotenv added

--- config/settings.py
+++ config/settings.py
@@ -13,9 +13,11 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
