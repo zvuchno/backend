1cb801b505cb2fcc77fcb7faabf9085e4ae3e550
Ilya Serkov <ilyasrkv22n@gmail.com>
2026-04-01 16:48:10 +0300

    add base crud for merch

--- store/serializers/image.py
+++ store/serializers/image.py
@@ -6,6 +6,9 @@ from store.models import Image
 class ImageSerializer(serializers.ModelSerializer):
     """Сериализатор для изображений мерча."""
 
+    image = serializers.ImageField()
+    is_main = serializers.BooleanField(required=False, default=False)
+
     class Meta:
         model = Image
-        fields = ('image', 'merch')
+        fields = ('image', 'is_main')
--- store/serializers/merch.py
+++ store/serializers/merch.py
@@ -1,12 +1,11 @@
+from decimal import Decimal
+
+from django.db import transaction
 from rest_framework import serializers
 
-from store.models import Merch
-from store.models.album import Album
-from store.models.image import Image
-from store.models.merch import Merch
+from store.constants import MAX_PRICE_DIGITS, PRICE_DECIMAL_PLACES
+from store.models import Image, Merch, Product, ProductVariant
 from store.serializers import ImageSerializer
-from store.serializers.album_merch import AlbumMerchSerializer
-from store.serializers.image import ImageSerializer
 
 
 class MerchReadSerializer(serializers.ModelSerializer):
@@ -68,6 +67,16 @@ class MerchDetailSerializer(MerchReadSerializer):
 class MerchWriteSerializer(serializers.ModelSerializer):
     """Сериализатор для создания и обновления Merch."""
 
+    price = serializers.DecimalField(
+        max_digits=MAX_PRICE_DIGITS,
+        decimal_places=PRICE_DECIMAL_PLACES,
+        required=True,
+        write_only=True,
+    )
+    allow_overpay = serializers.BooleanField(required=False)
+    stock = serializers.IntegerField(required=False)
+    images = ImageSerializer(many=True, write_only=True)
+
     class Meta:
         model = Merch
         fields = (
@@ -76,8 +85,79 @@ class MerchWriteSerializer(serializers.ModelSerializer):
             'price',
             'stock',
             'album',
-            'descriptions',
-            'image',
+            'description',
+            'images',
             'allow_overpay',
             'visibility',
+            'is_published',
+            'is_active',
         )
+
+    def validate_images(self, images):
+        main_count = sum(1 for image in images if image.get('is_main'))
+        if main_count > 1:
+            raise serializers.ValidationError(
+                'Только одно фото может быть главным.'
+            )
+        return images
+
+    def to_representation(self, instance):
+        return MerchDetailSerializer(instance).data
+
+    def create(self, validated_data):
+
+        price = validated_data.pop('price', None)
+        allow_overpay = validated_data.pop('allow_overpay', False)
+        stock = validated_data.pop('stock', 0)
+        images = validated_data.pop('images')
+
+        with transaction.atomic():
+            merch = Merch.objects.create(**validated_data)
+
+            for image in images:
+                Image.objects.create(
+                    merch=merch,
+                    image=image['image'],
+                    is_main=image.get('is_main', False),
+                )
+
+            product = Product.objects.create(
+                merch=merch,
+                price=(price if price is not None else Decimal('0.00')),
+                allow_overpay=allow_overpay,
+            )
+
+            ProductVariant.objects.create(
+                product=product,
+                stock=stock,
+                characteristic={'format': 'physical'},
+            )
+
+        return merch
+
+    def update(self, instance, validated_data):
+
+        price = validated_data.pop('price', None)
+        allow_overpay = validated_data.pop('allow_overpay', None)
+        stock = validated_data.pop('stock',  None)
+        validated_data.pop('images', None)
+
+        with transaction.atomic():
+
+            for attr, value in validated_data.items():
+                setattr(instance, attr, value)
+            instance.save(update_fields=[*validated_data.keys(), 'updated_at'])
+
+            product, _ = Product.objects.get_or_create(merch=instance)
+
+            if price is not None:
+                product.price = price
+            if allow_overpay is not None:
+                product.allow_overpay = allow_overpay
+
+            product.save()
+
+            if stock is not None:
+                ProductVariant.objects.filter(product=product).update(stock=stock)
+
+        return instance
