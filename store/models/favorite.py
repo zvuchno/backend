7160a3f5c207320c18e5662552aea5a02297d1a4
bobgoz <nikita.zaytsev2013@gmail.com>
2026-04-07 13:58:42 +0300

    favorite_model

---
+++ store/models/favorite.py
@@ -0,0 +1,43 @@
+"""
+Модуль модели Избранного.
+Позволяет пользователю отслеживать товары, которые он помечает как избранные.
+"""
+
+from django.contrib.auth import get_user_model
+from django.db import models
+
+from users.models.abstract.activatable_model import ActivatableModel
+from users.models.abstract.timestamp_model import TimestampModel
+
+User = get_user_model()
+
+
+class Favorite(ActivatableModel, TimestampModel):
+    """Модель избранного"""
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='favorites',
+        verbose_name='Пользователь',
+    )
+    product_variant = models.ForeignKey(
+        'store.ProductVariant',
+        verbose_name='Вариант продукта',
+        related_name='favorited_by',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name = 'избранное'
+        verbose_name_plural = 'избранные'
+        ordering = ('-created_at',)
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'product_variant'],
+                name='unique_favorite_per_user',
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.product_variant}'
