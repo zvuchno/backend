57920df0b03ffb1c25e9dd3311467c3a96b9ff72
Daniil Klochkov <kldaniil@gmail.com>
2026-04-17 18:36:14 +0500

    clean()

--- users/models/artist_legal_data.py
+++ users/models/artist_legal_data.py
@@ -1,5 +1,6 @@
 """Модель юридических и банковских данных артиста."""
 
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from .abstract import TimestampModel
@@ -13,6 +14,7 @@ from users.constants import (
     PASSPORT_ISSUED_BY_MAX_LENGTH,
     PASSPORT_NUMBER_MAX_LENGTH,
     PASSPORT_SERIES_MAX_LENGTH,
+    RECIPIENT_TYPE_MAX_LENGTH,
     TAXATION_FORM_MAX_LENGTH,
 )
 
@@ -24,10 +26,17 @@ class ArtistLegalProfile(TimestampModel):
     Используются для выплат, отчетности и договорной работы.
     """
 
-    class TaxationForm(models.TextChoices):
+    class TaxationSystem(models.TextChoices):
+        EMPTY = '', 'Не указано'
+        NPD = 'npd', 'НПД (самозанятый)'
+        USN = 'usn', 'УСН'
+        OSNO = 'osno', 'ОСНО'
+        PATENT = 'patent', 'Патент'
+
+    class RecipientType(models.TextChoices):
+        INDIVIDUAL = 'individual', 'Физическое лицо'
         SELF_EMPLOYED = 'self_employed', 'Самозанятый'
         INDIVIDUAL_ENTREPRENEUR = 'individual_entrepreneur', 'ИП'
-        INDIVIDUAL = 'individual', 'Физическое лицо'
         LEGAL_ENTITY = 'legal_entity', 'Юридическое лицо'
 
     artist = models.OneToOneField(
@@ -108,7 +117,7 @@ class ArtistLegalProfile(TimestampModel):
     )
     correspondent_account = models.CharField(
         'Корреспондентский счет',
-        max_length=20,
+        max_length=ACCOUNT_NUMBER_MAX_LENGTH,
         blank=True,
         default='',
     )
@@ -122,7 +131,13 @@ class ArtistLegalProfile(TimestampModel):
     taxation_form = models.CharField(
         'Форма налогообложения',
         max_length=TAXATION_FORM_MAX_LENGTH,
-        choices=TaxationForm.choices,
+        choices=TaxationSystem.choices,
+    )
+
+    recipient_type = models.CharField(
+        'Тип получателя выплат',
+        max_length=RECIPIENT_TYPE_MAX_LENGTH,
+        choices=RecipientType.choices,
         blank=True,
         default='',
     )
@@ -133,10 +148,52 @@ class ArtistLegalProfile(TimestampModel):
         help_text='Данные проверены вручную.',
     )
 
+    def clean(self):
+        """Проверяет формат юридических и банковских реквизитов."""
+        errors = {}
+
+        if self.inn and not self.inn.isdigit():
+            errors['inn'] = 'ИНН должен содержать только цифры.'
+
+        if self.inn and len(self.inn) not in (10, 12):
+            errors['inn'] = 'ИНН должен содержать 10 или 12 цифр.'
+
+        if self.bik and (not self.bik.isdigit() or len(self.bik) != 9):
+            errors['bik'] = 'БИК должен содержать 9 цифр.'
+
+        if self.correspondent_account and (
+            not self.correspondent_account.isdigit()
+            or len(self.correspondent_account) != 20
+        ):
+            errors['correspondent_account'] = (
+                'Корреспондентский счет должен содержать 20 цифр.'
+            )
+
+        if self.checking_account and (
+            not self.checking_account.isdigit()
+            or len(self.checking_account) != 20
+        ):
+            errors['checking_account'] = (
+                'Расчетный счет должен содержать 20 цифр.'
+            )
+
+        if self.passport_series and not self.passport_series.isdigit():
+            errors['passport_series'] = (
+                'Серия паспорта должна содержать цифры.'
+            )
+
+        if self.passport_number and not self.passport_number.isdigit():
+            errors['passport_number'] = (
+                'Номер паспорта должен содержать цифры.'
+            )
+
+        if errors:
+            raise ValidationError(errors)
+
     class Meta:
         verbose_name = 'юридические данные артиста'
         verbose_name_plural = 'юридические данные артистов'
         ordering = ('-updated_at',)
 
     def __str__(self):
-        return f'Юридические данные: {self.artist.name}'
+        return f'Юридические данные артиста: {self.artist.name}'
