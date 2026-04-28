7b2626a8342695459d406a98173a763ed02e5ebf
Daniil Klochkov <kldaniil@gmail.com>
2026-04-28 16:23:30 +0500

    readme and mangement keygen commend fixed

--- users/management/commands/generate_encryption_key.py
+++ users/management/commands/generate_encryption_key.py
@@ -12,5 +12,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         key = Fernet.generate_key().decode()
         self.stdout.write(
-            self.style.SUCCESS(f'FIELD_ENCRYPTION_KEYS={key}'),
+            self.style.SUCCESS(
+                'Скопируйте и добавьте в .env в FIELD_ENCRYPTION_KEYS\n'
+                f'{key}\n'
+                'Если ключей несколько — новый должен быть первым (ротация).',
+            ),
         )
