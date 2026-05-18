2cd31145fbb90db2511c73eef91ed270710b07ca
Pavel <pavel.kulichenko.us@gmail.com>
2026-05-18 17:32:03 +0500

    refactor(cart): move querysets to a dedicated module (#272)

--- store/querysets/__init__.py
+++ store/querysets/__init__.py
@@ -1,9 +1,13 @@
 """Модуль QuerySet для работы с правилами доступа и выборкой данных."""
 
+from .cart_items import CartItemQuerySet
+from .carts import CartQuerySet
 from .track_visibility import TrackQuerySet
 from .visibility import VisibilityQuerySet
 
 __all__ = [
+    'CartQuerySet',
+    'CartItemQuerySet',
     'VisibilityQuerySet',
     'TrackQuerySet',
 ]
