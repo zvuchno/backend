88d503ecee76dc0b6ad71bb726b91ad7ae28c30c
Pavel <pavel.kulichenko.us@gmail.com>
2026-06-08 20:40:25 +0500

    feat(cart): unify fields (#325)

--- store/querysets/__init__.py
+++ store/querysets/__init__.py
@@ -4,9 +4,11 @@ from .cart_items import CartItemQuerySet
 from .carts import CartQuerySet
 from .product import ProductQuerySet
 from .track_visibility import TrackQuerySet
+from .variant_annotations import build_target_annotations
 from .visibility import VisibilityQuerySet
 
 __all__ = [
+    'build_target_annotations',
     'CartQuerySet',
     'CartItemQuerySet',
     'VisibilityQuerySet',
