578f7093edf75d763f0b83e371d9102383293533
Куличенко Павел <pavel.kulichenko@gmail.com>
2026-06-26 20:31:27 +0500

    feat(admin): add admin logging (#400)

---
+++ common/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class CommonConfig(AppConfig):
+    """Конфигурация приложения для общих компонентов проекта."""
+
+    name = 'common'
+    verbose_name = 'Общие компоненты'
