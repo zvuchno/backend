9c9e2ee340eae7cc099e85d947fd0a26950eb419
Pavel Kulichenko <pavel.kulichenko@gmail.com>
2026-07-02 12:45:49 +0500

    feat(PaymentView): add permission_classes

--- store/views/payment.py
+++ store/views/payment.py
@@ -5,6 +5,7 @@ from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from yookassa.domain.common import SecurityHelper
@@ -27,6 +28,8 @@ class CreatePaymentView(APIView):
     платежа через платежный шлюз и возвращает URL для перенаправления
     пользователя на страницу оплаты.
     """
+
+    permission_classes = (IsAuthenticated,)
 
     def post(self, request):
         order_id = request.data.get('order_id')
