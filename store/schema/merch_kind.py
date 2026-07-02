6816851cfdbd893e274ea318f5daf02cc4c92f38
Pavel Kulichenko <pavel.kulichenko@gmail.com>
2026-07-02 12:25:09 +0500

    ref(schema): move merch kind

--- store/schema/merch_kind.py
+++ store/schema/merch_kind.py
@@ -13,6 +13,6 @@ merch_kinds_schema = extend_schema_view(
     list=extend_schema(
         summary='Типы мерча',
         description='Возвращает список типов мерча.',
-        tags=['Merch kinds'],
+        tags=['Merch'],
     ),
 )
