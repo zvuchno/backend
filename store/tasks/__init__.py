94a0a883efee29e615a7fdd94dc3244add7c6be3
Daniil Klochkov <kldaniil@gmail.com>
2026-07-20 17:38:34 +0500

    init import

--- store/tasks/__init__.py
+++ store/tasks/__init__.py
@@ -3,8 +3,10 @@ from .audio import prepare_track_audio
 from .cdek import register_cdek_orders_task, update_cdek_shipment_task
 from .reservations import release_expired_reservations
 from .telegram import send_telegram_notification
+from .track_upload import cleanup_expired_track_uploads
 
 __all__ = [
+    'cleanup_expired_track_uploads',
     'prepare_track_audio',
     'build_album_archive',
     'register_cdek_orders_task',
