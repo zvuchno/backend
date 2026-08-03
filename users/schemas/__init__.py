99e7cd2a274461818fe75f77bf7ce23bb0b7d05f
Даниил Клочков <kldaniil@gmail.com>
2026-08-03 14:58:57 +0500

    feat/526-label-can-create-artist (#537)
    * cover
    
    * me view and me/managed/
    
    * create artist by label
    
    * artist create
    
    * user upgrade midified
    
    * test fixes
    
    * select_related deleted from ArtistProfileBaseView

--- users/schemas/__init__.py
+++ users/schemas/__init__.py
@@ -27,6 +27,8 @@ from .artist_profile import (
     artist_me_schema,
     artist_public_schema,
     label_managed_profile_list_schema,
+    managed_artist_cover_update_schema,
+    managed_artist_schema,
 )
 from .auth import (
     logout_schema,
@@ -66,6 +68,8 @@ __all__ = [
     'listener_me_schema',
     'listener_registration_schema',
     'logout_schema',
+    'managed_artist_cover_update_schema',
+    'managed_artist_schema',
     'me_schema',
     'password_reset_confirm_schema',
     'password_reset_request_schema',
