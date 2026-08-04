65d32a0282ed47120b43d3c456773c786e3fe5d9
Daniil Klochkov <kldaniil@gmail.com>
2026-08-04 15:52:08 +0500

    run_actions_after_authentication moved to view mixin

--- users/serializers/cookie_auth.py
+++ users/serializers/cookie_auth.py
@@ -1,24 +1,12 @@
 from dj_rest_auth.serializers import LoginSerializer
 from rest_framework import serializers
 
-from users.helpers import run_actions_after_authentication
-
 
 class CookieLoginSerializer(LoginSerializer):
     """Сериализатор входа в cookie-сессию."""
 
     username = None
     email = serializers.EmailField(required=True)
-
-    def validate(self, attrs):
-        validated_data = super().validate(attrs)
-
-        run_actions_after_authentication(
-            validated_data['user'],
-            self.context['request'],
-        )
-
-        return validated_data
 
 
 class CookieLoginResponseSerializer(serializers.Serializer):
--- users/views/mixins/cookie_auth.py
+++ users/views/mixins/cookie_auth.py
@@ -2,12 +2,20 @@ from django.middleware.csrf import get_token
 from rest_framework import status
 from rest_framework.response import Response
 
+from users.helpers import run_actions_after_authentication
+
 
 class CookieResponseMixin:
     """Возвращает единый ответ и устанавливает CSRF-cookie после входа."""
 
     def get_response(self) -> Response:
         response = super().get_response()
+
+        run_actions_after_authentication(
+            self.user,
+            self.request,
+        )
+
         get_token(self.request)
         response.data = {
             'authenticated': True,
