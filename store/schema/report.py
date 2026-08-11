fc8202127dd08f6e1936fb50d69a95a9146a46ab
Pavel Kulichenko <pavel.kulichenko@gmail.com>
2026-08-11 20:40:05 +0500

    feat(report): update PDF download

--- store/schema/report.py
+++ store/schema/report.py
@@ -1,27 +1,28 @@
-"""Схемы автодокументации OpenAPI для финансовых отчетов артистов.
+"""Схемы автодокументации для агентских отчетов артистов.
 
 Содержит конфигурации `drf-spectacular` для отображения
-операций получения отчетов в Swagger/ReDoc.
+операций получения и скачивания агентских отчетов
+в Swagger/ReDoc.
 """
 
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import (
     OpenApiExample,
     OpenApiParameter,
+    OpenApiResponse,
     extend_schema,
     extend_schema_view,
 )
 
 ARTIST_REPORTS_TAGS = ['Reports']
 
-
 artist_reports_schema = extend_schema_view(
     list=extend_schema(
-        summary='Список финансовых отчетов артиста',
+        summary='Отчеты агента для артиста',
         description=(
-            'Возвращает список сформированных финансовых отчетов '
+            'Возвращает список сформированных агентских отчетов '
             'текущего артиста с агрегированными данными о продажах '
-            'за выбранные периоды.'
+            'за отчетные периоды.'
         ),
         tags=ARTIST_REPORTS_TAGS,
         parameters=[
@@ -50,13 +51,27 @@ artist_reports_schema = extend_schema_view(
             OpenApiParameter(
                 name='limit',
                 type=OpenApiTypes.INT,
-                description='Количество элементов в ответе',
+                description='Количество элементов в ответе.',
             ),
             OpenApiParameter(
                 name='offset',
                 type=OpenApiTypes.INT,
-                description='Смещение от начала выборки',
+                description='Смещение от начала выборки.',
             ),
         ],
+    ),
+    download=extend_schema(
+        summary='Скачать отчет агента ',
+        description=('Возвращает PDF-файл отчета агента текущего артиста. '),
+        tags=ARTIST_REPORTS_TAGS,
+        responses={
+            200: OpenApiResponse(
+                response=OpenApiTypes.BINARY,
+                description='PDF-файл отчета агента.',
+            ),
+            404: OpenApiResponse(
+                description='Отчет или файл отчета не найден.',
+            ),
+        },
     ),
 )
